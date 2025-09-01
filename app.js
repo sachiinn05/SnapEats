3169faca26e96c10174128c532119ebeb0d538fc
sachinsingh <sachinsingh6386@gmail.com>
2025-09-01 16:56:00 +0530

    add resturrent card

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-//JSX (transpiled before ir reaches the JS)-PARCEL-Babel
-//? JSX => Bable transpiled it to  React.createElement=>ReactElement-JS Oblect=>HTMLElement(render)
+const Header=()=>{
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://static.vecteezy.com/system/resources/previews/011/405/724/non_2x/call-food-logo-design-food-delivery-service-logo-concept-free-vector.jpg"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
 
-const jsxHeading=(<h2 className="head" tabIndex={5}>Namaste React using JSX</h2>);
+            </div>
+        </div>
+    )
+};
 
-
-//?? React Functional Component
-//*here is <Title/> is example of component Composition;
-const Title=()=>(
-    <h1 className="head" tabIndex={5}>Namaste React Title </h1>
-);
-const HeadingComponent=()=>(
-    <div id="container">
-        <Title/>
-        <h1 className="heading">Namaste React from functional component</h1>
+const ResturantCard=()=>{
+    return (
+  <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+    <img  className="res-logo"
+    alt="res-logo"src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/4/30/4ea4293b-557f-4b8c-8ecb-4352bfed28a3_3a2a51fe-a707-4974-866d-d5ff72ddd0ca.jpeg"></img>
+    <h3>BBQ</h3>
+    <h4>Chicken Roll,North Indian</h4>
+    <h4>4.6 Star</h4>
+    <h4>45 min</h4>
+  </div>
+    );
+};
+const Body=()=>{
+return(
+    <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+            <ResturantCard/>
+             <ResturantCard/>
+              <ResturantCard/>
+               <ResturantCard/>
+                <ResturantCard/>
+                 <ResturantCard/>
+             <ResturantCard/>
+              <ResturantCard/>
+               <ResturantCard/>
+              <ResturantCard/>
+               <ResturantCard/>
+                <ResturantCard/>
+                 <ResturantCard/>
+                  <ResturantCard/>
+                   <ResturantCard/>
+                    <ResturantCard/>
+                     <ResturantCard/>
+                      <ResturantCard/>
+        </div>
     </div>
 )
 
-const root =ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
-root.render(<HeadingComponent/>)
+}
+const AppLayout=()=>{
+    return <div className="app">
+        <Header/>
+        <Body/>
+    </div>;
+};
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>)
 
    
    
